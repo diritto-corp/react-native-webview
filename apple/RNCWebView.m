e3a4b54908a88ae038b6fb56ed02ff2091674ae5
junheongpark <junheong.park@ridi.com>
2023-04-20 15:14:39 +0900

    디리토 tintColor 지정

--- apple/RNCWebView.m
+++ apple/RNCWebView.m
@@ -146,6 +146,7 @@ RCTAutoInsetsProtocol>
     super.backgroundColor = [UIColor clearColor];
 #else
     super.backgroundColor = [RCTUIColor clearColor];
+    super.tintColor = [UIColor colorWithRed: 0.57 green: 0.37 blue: 1.00 alpha: 1.00];
 #endif // !TARGET_OS_OSX
     _bounces = YES;
     _scrollEnabled = YES;
